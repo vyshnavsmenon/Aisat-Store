6ac8aa77141ead3fb1604051642b5dfe51fdc07c
vyshnavsmenon <vyshnavsmenon62@gmail.com>
2023-10-11 00:15:06 +0530

    first-commit

---
+++ src/Login.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Loader from './Loader';
+import './Login.css';
+
+function Login() {
+    const [isLoading, setIsLoading] = useState(false);
+  return (
+    <div className='background'>         
+            <div className='small-body'>
+                
+            </div>
+        
+    </div>
+  )
+}
+
+export default Login
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/vyshnavsmenon/Aisat-Store.git
+// git push -u origin main
---
+++ src/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Navbar.css';
+import {Link, useNavigate} from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='first-portion'>
+            <div className='logo'>
+                <label>ALBERTIAN INSTITUTE OF SCIENCE AND TECHNOLOGY(AISAT)</label>
+            </div>
+        </div>
+        <div className='last-portion'>
+            <ul>
+                <li>
+                    <Link className='list'>Home</Link>
+                </li>
+                <li>
+                    <Link className='list'>Login</Link>
+                </li>
+                <li>
+                    <Link className='list'>Sign Up</Link>
+                </li>
+                <li>
+                    <Link className='list'>Profile</Link>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
